b6716586c4e12b26f2a0ba6900bd1965d832ac9a
Parker <s204814@student.dtu.dk>
2022-02-22 11:04:14 +0100

    Nu med sej Gui part 2

---
+++ src/main/java/GuiController.java
@@ -0,0 +1,11 @@
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Polyline;
+
+public class GuiController {
+    public Polyline poly;
+    public Label label2;
+
+    public void buttonPressed(MouseEvent mouseEvent) {
+    }
+}
--- src/main/java/JavaFx.java
+++ src/main/java/JavaFx.java
@@ -1,5 +1,7 @@
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.application.Application;
 import javafx.scene.control.Label;
@@ -13,20 +15,8 @@ public class JavaFx extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        String version = System.getProperty("javafx.version");
-        Label label = new Label("Hello from Javafx" + version);
-        TextField hey = new TextField("Heste, heste, Parker er den bedste!");
-        Label label1 = new Label("Hello from JavaFX: Commit nr 21 " + version);
-        Label label2 = new Label("Hej IT 62450!");
-        Label label3 = new Label("Hej IT 62450!");
-        GridPane pane = new GridPane();
-        pane.setGridLinesVisible(true);
-        pane.add(label1,0,0);
-        pane.add(label2,1,1);
-        pane.add(label3,2,2);
-        //stackPane.getChildren().add(new Label(" hey"));
-        //Scene scene = new Scene(stackPane, 640, 480);
-        //stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Gui.fxml"));
+        FlowPane pane = loader.load();
         Scene scene = new Scene(pane, 240, 100);
         stage.setScene(scene);
         stage.show();
