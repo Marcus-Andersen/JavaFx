52b5bbc1ba89833c35a3508c66ca164ce2f5e0f9
Parker <s204814@student.dtu.dk>
2022-03-22 11:19:00 +0100

    EKGSimulator klasse

--- src/main/java/GuiController.java
+++ src/main/java/GuiController.java
@@ -8,23 +8,10 @@ import javafx.scene.shape.Polyline;
 import javafx.stage.Stage;
 
 public class GuiController {
-    public Polyline poly;
+    //public Polyline poly;
     public Label label2;
 
-    public void buttonPressed(MouseEvent mouseEvent) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (double i = -15; i < 60; i++) {
-                    poly.getPoints().addAll(i*10,Math.random()*300);
-                    try{
-                        Thread.sleep(500);
-                    }
-                    catch (Exception e){}
-                }
-
-
-            }
-        }).start();
+    public void ekgSim(MouseEvent mouseEvent) {
+        EKGSimulator.measure();
     }
 }
