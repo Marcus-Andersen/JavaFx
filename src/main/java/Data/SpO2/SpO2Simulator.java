1e7f6eb1856732adc143257229e16413cab4a2de
Parker <s204814@student.dtu.dk>
2022-04-05 10:23:08 +0200

    SpO2-branch 2

--- src/main/java/Data/SpO2/SpO2Simulator.java
+++ src/main/java/Data/SpO2/SpO2Simulator.java
@@ -6,7 +6,12 @@ import Data.Sims;
 public class SpO2Simulator implements Sims {
     @Override
     public void record() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
     }
 
     @Override
